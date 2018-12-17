f361f8f707e6ee7c01221c30b64d7809785b10db
FlorentP42 <45787476+FlorentP42@users.noreply.github.com>
2018-12-17 11:20:36 +0100

    Made check more generic by disabling tests if cross-building

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -16,13 +16,12 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        arm_archs = ['arm', 'aarch64_be', 'aarch64', 'armv8b', 'armv8l']
-        if "arm" in self.settings.arch:
-            if platform.machine() in arm_archs:
-                self.test_arm()
-        else:
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+        if not tools.cross_building(self.settings):
+            if "arm" in self.settings.arch:
+                    self.test_arm()
+            else:
+                bin_path = os.path.join("bin", "test_package")
+                self.run(bin_path, run_environment=True)
 
     def test_arm(self):
         file_ext = "so" if self.options["libpng"].shared else "a"
